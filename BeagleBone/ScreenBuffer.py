832d5945a346ddec0910853907ae43dcad2c1e0c
Kurt Mohler <kurtamohler@gmail.com>
2014-10-08 15:52:18 -0400

    Added more to Sender.py and ScreenBuffer.py

--- BeagleBone/ScreenBuffer.py
+++ BeagleBone/ScreenBuffer.py
@@ -11,6 +11,16 @@ class ScreenBuffer:
         self.mX = x
         self.mY = y
 
+    # Set a pixel's color.
+    def Set(self, x, y, red, green, blue):
+        pass
+        #! set pixel (x, y) to (red, green, blue)
+
+    # Clear the ScreenBuffer.
+    def Clear(self):
+        pass
+        #! delete and reconstruct the array of pixels
+
 #Tester
 if __name__ == "__main__":
     screen = ScreenBuffer()
--- BeagleBone/Sender.py
+++ BeagleBone/Sender.py
@@ -12,6 +12,8 @@ from ScreenBuffer import ScreenBuffer
 
 UART.setup(uart)
 
+# This class is used to send the data in a ScreenBuffer across UART
+# byte by byte.
 class Sender:
     # The UART which we use.
     # RX -> P9_26
@@ -24,26 +26,39 @@ class Sender:
     mSerial = ""
 
     # The current screen buffer to send.
-    mScreenBuffer = ScreenBuffer()
+    mScreenBuffer = ""
 
     # Constructor
+    # Initializes the ScreenBuffer and sets up the UART serial interface.
     def __init__(self):
         self.mScreenBuffer = ScreenBuffer()
 
         UART.setup(Sender.mUart)
 
-        mSerial = serial.Serial(port = mUartDev, buadrate = mBaudRate)
-        mSerial.close()
-        mSerial.open()
-        if serial.isOpen():
-            mSerial.write("Beaglebone serial is open.")
+        self.mSerial = serial.Serial(port = mUartDev, buadrate = mBaudRate)
+        self.mSerial.close()
+        self.mSerial.open()
+        if self.mSerial.isOpen():
+            self.mSerial.write("Beaglebone serial is open.")
 
-    # Set the ScreenBuffer to send.
+    # Destructor
+    # Cleans up data and closes the UART serial interface after
+    # sending a blank ScreenBuffer.
+    def __del__(self):
+        # Get a blank ScreenBuffer.
+        self.mScreenBuffer = ScreenBuffer()
+        self.SendScreenBuffer()
+
+        self.mSerial.close()
+
+
+    # Set a new ScreenBuffer.
     def SetScreenBuffer(self, screenBuffer):
+        del self.mScreenBuffer
         self.mScreenBuffer = screenBuffer
 
     # Send the data from a ScreenBuffer over UART.
-    def SendScreenBuffer(self):
+    def Display(self):
         pass
         # Pseudo-code implementation
         #! mSerial.write(TX begin command)
@@ -52,11 +67,14 @@ class Sender:
         #!     mSerial.write(character)
         #! mSerial.write(TX end command)
 
+
+
 # Tester
 if __name__ == "__main__":
     sender = Sender()
     print sender.mScreenBuffer.mX
     screenBuffer = ScreenBuffer(10, 10)
     sender.SetScreenBuffer(screenBuffer)
+    sender.Display()
     print sender.mScreenBuffer.mX
-
+    del sender
