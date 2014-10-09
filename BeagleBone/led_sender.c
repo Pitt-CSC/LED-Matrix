4f5392d387046374f10320266e1b432a68cb8420
Matthew Gruda <matthewgruda@gmail.com>
2014-10-08 21:11:32 -0400

    Some work on led_sender impl. Some on TX protocol.
    	*led_sender.c is the primary file for the impl.
    	*led_sender_private.h contains private stuff for above
    	*BB_ARD_transmission_protocol will contain documentation
    		for the BeagleBone to Arduino protocol.

---
+++ BeagleBone/led_sender.c
@@ -0,0 +1,26 @@
+/*
+ * =============================================================================
+ *
+ *       Filename:  led_sender.c
+ *
+ *    Description:  Implementation of led_sender.h
+ *
+ *        Version:  1.0
+ *        Created:  10/06/14 16:14:58
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Matthew Gruda (mg), matthewgruda@gmail.com
+ *   Organization:  
+ *
+ * =============================================================================
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <termios.h>
+
+int led_matrix_init(unsigned int flags){
+	
+}
+
---
+++ BeagleBone/led_sender_private.h
@@ -0,0 +1,28 @@
+/*
+ * =============================================================================
+ *
+ *       Filename:  led_sender_private.h
+ *
+ *    Description:  Private header file for the led_sender, contains all 
+ *    			defines used by led_sender not in the public interface
+ *
+ *        Version:  1.0
+ *        Created:  10/06/14 16:10:06
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Matthew Gruda (mg), matthewgruda@gmail.com
+ *   Organization:  
+ *
+ * =============================================================================
+ */
+
+#ifndef __LED_SENDER_PRIVATE_H
+#define __LED_SENDER_PRIVATE_H
+
+#include "led_sender.h"
+
+#define DEV_PATH	"/dev/ttyO1"
+
+#endif 	/* __LED_SENDER_PRIVATE_H */
+
